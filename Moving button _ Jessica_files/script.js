da8c144e267a1c41e26b12c1cbc89b647431d82b
caotuankhainguyen <100828110+caotuankhainguyen@users.noreply.github.com>
2024-03-25 15:23:02 +0700

    Update script.js

--- Moving button _ Jessica_files/script.js
+++ Moving button _ Jessica_files/script.js
@@ -6,7 +6,7 @@ const noBtn = document.querySelector(".no-btn");
 const questionPrompt = document.querySelector(".question-prompt"); // Thêm đoạn này
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Anh đẹp trai nhất trái đất<br>Yêuuuuu x10000000";
+  question.innerHTML = "Anh yêu bé nhất trái đất<br>Yêuuuuu bé x10000000";
   gif.src =
     "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExc3R0M3NnazRsdXF2b3hjdXlydXZneGQxZm80cG9id2Q1bGw4aTRlMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/AcfTF7tyikWyroP0x7/giphy.gif";
 
